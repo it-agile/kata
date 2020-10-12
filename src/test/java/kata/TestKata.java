946d9692dec7ef685dc4efef3d7a60c37d647ee2
Holger Bohlmann <holger.bohlmann@it-agile.de>
2020-10-12 13:41:28 +0200

    Hamcrest

--- src/test/java/kata/TestKata.java
+++ src/test/java/kata/TestKata.java
@@ -2,11 +2,13 @@ package kata;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestKata {
 
     @Test
     public void testSomething() {
-        assertEquals(1,2);
+        assertThat("Nein", equalTo("Ja"));
     }
 }
