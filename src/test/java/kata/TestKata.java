35510949eb9611933e46ddd4326f46eb596d2215
Holger Bohlmann <holger.bohlmann@it-agile.de>
2020-10-12 13:38:18 +0200

    First Test

---
+++ src/test/java/kata/TestKata.java
@@ -0,0 +1,12 @@
+package kata;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestKata {
+
+    @Test
+    public void testSomething() {
+        assertEquals(1,2);
+    }
+}
